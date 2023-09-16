823d114d7b082ca56170af1352b0456d8923e64a
MannShah17 <mannshah2004@gmail.com>
2023-09-16 12:12:03 +0530

    added comments

--- controllers/employeeControllers.js
+++ controllers/employeeControllers.js
@@ -1,157 +1,49 @@
 const fs = require('fs');
 const path = require('path');
 
+// Define the file path to store employee data
 const employeesFilePath = path.join(__dirname, '../data', '/data.json');
 
+// Function to retrieve all employees
 const getAllEmployees = (req, res) => {
-  fs.readFile(employeesFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
-      return;
-    }
-
-    const employees = JSON.parse(data);
-    res.status(200).json(employees);
-  });
+  // Use fs.readFile to read data from the file
+  // Parse the data, handle errors, and send the response
 };
 
+// Function to retrieve an employee by ID
 const getEmployeeById = (req, res) => {
-  const employeeId = parseInt(req.params.id);
-
-  fs.readFile(employeesFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
-      return;
-    }
-
-    const employees = JSON.parse(data);
-    const employee = employees.find((emp) => emp.id === employeeId);
-
-    if (!employee) {
-      res.status(404).json({ message: 'Employee not found' });
-      return;
-    }
-
-    res.status(200).json(employee);
-  });
+  // Extract employee ID from request parameters
+  // Use fs.readFile to read data from the file
+  // Find the employee with the given ID, handle errors, and send the response
 };
 
+// Function to create a new employee
 const createEmployee = (req, res) => {
-  const { name, email, position } = req.body;
-
-  fs.readFile(employeesFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
-      return;
-    }
-
-    const employees = JSON.parse(data);
-    const newEmployee = {
-      id: generateEmployeeId(employees),
-      name,
-      email,
-      position,
-    };
-    employees.push(newEmployee);
-
-    fs.writeFile(employeesFilePath, JSON.stringify(employees), (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal server error' });
-        return;
-      }
-
-      res
-        .status(201)
-        .json({ message: 'Employee created', employee: newEmployee });
-    });
-  });
+  // Extract employee data from request body
+  // Use fs.readFile to read data from the file
+  // Create a new employee object, add it to the data, and write back to the file
+  // Handle errors and send the response
 };
 
+// Function to update an employee by ID
 const updateEmployee = (req, res) => {
-  const employeeId = parseInt(req.params.id);
-  const { name, email, position } = req.body;
-
-  fs.readFile(employeesFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
-      return;
-    }
-
-    let employees = JSON.parse(data);
-    const employeeIndex = employees.findIndex((emp) => emp.id === employeeId);
-
-    if (employeeIndex === -1) {
-      res.status(404).json({ message: 'Employee not found' });
-      return;
-    }
-
-    employees[employeeIndex] = {
-      ...employees[employeeIndex],
-      name,
-      email,
-      position,
-    };
-
-    fs.writeFile(employeesFilePath, JSON.stringify(employees), (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal server error' });
-        return;
-      }
-
-      res.status(200).json({
-        message: 'Employee updated',
-        employee: employees[employeeIndex],
-      });
-    });
-  });
+  // Extract employee ID and updated data from request parameters and body
+  // Use fs.readFile to read data from the file
+  // Find the employee with the given ID, update its data, and write back to the file
+  // Handle errors and send the response
 };
 
+// Function to delete an employee by ID
 const deleteEmployee = (req, res) => {
-  const employeeId = parseInt(req.params.id);
-
-  fs.readFile(employeesFilePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal server error' });
-      return;
-    }
-
-    let employees = JSON.parse(data);
-    const employeeIndex = employees.findIndex((emp) => emp.id === employeeId);
-
-    if (employeeIndex === -1) {
-      res.status(404).json({ message: 'Employee not found' });
-      return;
-    }
-
-    const deletedEmployee = employees.splice(employeeIndex, 1);
-
-    fs.writeFile(employeesFilePath, JSON.stringify(employees), (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal server error' });
-        return;
-      }
-
-      res
-        .status(200)
-        .json({ message: 'Employee deleted', employee: deletedEmployee });
-    });
-  });
+  // Extract employee ID from request parameters
+  // Use fs.readFile to read data from the file
+  // Find the employee with the given ID, delete it, and write back to the file
+  // Handle errors and send the response
 };
 
+// Function to generate a unique employee ID
 const generateEmployeeId = (employees) => {
-  const maxId = employees.reduce(
-    (max, emp) => (emp.id > max ? emp.id : max),
-    0
-  );
-  return maxId + 1;
+  // Calculate and return a new unique ID based on existing employees
 };
 
 module.exports = {
